ba7f7fd09a88aaaa7c3a33cc14cdfcdbd78ba417
Akash Kedia <kedia23akash@gmail.com>
2021-11-01 22:33:02 -0700

    Finished auth flow

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,59 @@
-from flask import Flask
+import firebase_admin
+import pyrebase
+import json
+from firebase_admin import credentials, auth
+from flask import Flask, request
+from functools import wraps
+import logging
 
 app = Flask(__name__)
 
+cred = credentials.Certificate('fbAdminConfig.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('fbconfig.json')))
+
+def check_token(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if not request.headers.get('authorization'):
+            return {'message': 'No token provided'},400
+        try:
+            user = auth.verify_id_token(request.headers['authorization'])
+            request.user = user
+        except:
+            return {'message':'Invalid token provided.'},400
+        return f(*args, **kwargs)
+    return wrap
+
+#Api route to sign up a new user
+@app.route('/api/signup')
+def signup():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    print(email)
+    print(password)
+    if email is None or password is None:
+        return {'message': 'Error missing email or password'},400
+    try:
+        user = auth.create_user(
+               email=email,
+               password=password
+        )
+        return {'message': f'Successfully created user {user.uid}'},200
+    except:
+        logging.exception("message")
+        return {'message': 'Error creating user'},400
+#Api route to get a new token for a valid user
+@app.route('/api/token')
+def token():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = pb.auth().sign_in_with_email_and_password(email, password)
+        jwt = user['idToken']
+        return {'token': jwt},200
+    except:
+        return {'message': 'There was an error logging in'},400
 
 @app.route("/")
 def hello():
@@ -9,4 +61,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
