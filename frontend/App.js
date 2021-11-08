7fd5565f78fc07e6827cab5d73c245cf4935ccf1
Ria Bhatt <rbhatt01@calpoly.edu>
2021-11-07 16:37:40 -0800

    nav

--- frontend/App.js
+++ frontend/App.js
@@ -8,10 +8,19 @@ import firebase from "firebase";
 
 import { Card, IconButton, OverlayLabel } from "./src/components";
 import styles from "./App.styles";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const handleSwipeRight = (cardIndex) => {};
 
-const App = () => {
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function LandingPage() { 
   const [cardIndex, setCardIndex] = useState(0);
   const [leftEnlargedImage, setLeftEnlargedImage] = useState("DEFAULT");
   const [rightEnlargedImage, setRightEnlargedImage] = useState("DEFAULT");
@@ -30,127 +39,121 @@ const App = () => {
     console.log(photoCards[cardIndex].name);
     setCardIndex(cardIndex + 1);
   };
-
   return (
     <View>
-      {leftEnlargedImage === "TAPPED" && (
-        <View style={{ position: "absolute", zIndex: 2 }}>
-          <TouchableOpacity
-            onPress={() => {
-              setLeftEnlargedImage("DEFAULT");
-              // setEnlargedImage("DEFAULT");
-            }}
-          >
-            <Image source={photoCards[cardIndex % photoCards.length].photo} />
-          </TouchableOpacity>
-        </View>
-      )}
-      {rightEnlargedImage === "TAPPED" && (
-        <View style={{ position: "absolute", zIndex: 2 }}>
-          <TouchableOpacity
-            onPress={() => {
-              setRightEnlargedImage("DEFAULT");
-              // setEnlargedImage("DEFAULT");
-            }}
-          >
-            <Image
-              resizeMode={"contain"}
-              source={photoCards2[cardIndex % photoCards2.length].photo}
-            />
-          </TouchableOpacity>
-        </View>
-      )}
-      {/* {enlargedImage === "DEFAULT" && ( */}
-      <View
-        style={[styles.swiperContainer, { position: "absolute", zIndex: 0 }]}
-      >
-        <View style={[styles.leftSwiper, { zIndex: 0 }]}>
-          <Swiper
-            ref={useSwiperL}
-            animateCardOpacity
-            // containerStyle={styles.container}
-            cards={photoCards}
-            renderCard={(card) => <Card card={card} />}
-            cardIndex={cardIndex}
-            backgroundColor="white"
-            stackSize={2}
-            infinite
-            showSecondCard
-            disableLeftSwipe
-            disableBottomSwipe
-            animateOverlayLabelsOpacity
-            onTapCard={() => {
-              setLeftEnlargedImage("TAPPED");
-              // setEnlargedImage("TAPPED");
-            }}
-            onSwipedRight={handleOnSwipedRight}
-            overlayLabels={{
-              right: {
-                title: "LIKE",
-                element: <OverlayLabel label="LIKE" color="#4CCC93" />,
-              },
-            }}
-          />
-        </View>
-        <View style={(styles.rightSwiper, { zIndex: 0 })}>
-          <Swiper
-            ref={useSwiperR}
-            animateCardOpacity
-            // containerStyle={styles.container}
-            cards={photoCards2}
-            renderCard={(card) => <Card card={card} />}
-            cardIndex={cardIndex}
-            backgroundColor="white"
-            stackSize={2}
-            infinite
-            showSecondCard
-            disableRightSwipe
-            disableBottomSwipe
-            animateOverlayLabelsOpacity
-            onTapCard={() => {
-              setRightEnlargedImage("TAPPED");
-              // setEnlargedImage("TAPPED");
-            }}
-            onSwipedLeft={handleOnSwipedLeft}
-            overlayLabels={{
-              right: {
-                title: "LIKE",
-                element: <OverlayLabel label="NOPE" color="#E5566D" />,
-                style: {
-                  wrapper: styles.overlayWrapper,
-                },
-              },
-              left: {
-                title: "LIKE",
-                element: <OverlayLabel label="LIKE" color="#4CCC93" />,
-              },
-            }}
-          />
-        </View>
+    {leftEnlargedImage === "TAPPED" && (
+      <View style={{ position: "absolute", zIndex: 2 }}>
+        <TouchableOpacity
+          onPress={() => {
+            setLeftEnlargedImage("DEFAULT");
+            // setEnlargedImage("DEFAULT");
+          }}
+        >
+          <Image source={photoCards[cardIndex % photoCards.length].photo} />
+        </TouchableOpacity>
       </View>
-      {/* <View style={styles.buttonsContainer}>
-        <IconButton
-          name="heart"
-          onPress={handleOnSwipedRight}
-          color="white"
-          backgroundColor="#E5566D"
+    )}
+    {rightEnlargedImage === "TAPPED" && (
+      <View style={{ position: "absolute", zIndex: 2 }}>
+        <TouchableOpacity
+          onPress={() => {
+            setRightEnlargedImage("DEFAULT");
+            // setEnlargedImage("DEFAULT");
+          }}
+        >
+          <Image
+            resizeMode={"contain"}
+            source={photoCards2[cardIndex % photoCards2.length].photo}
+          />
+        </TouchableOpacity>
+      </View>
+    )}
+    {/* {enlargedImage === "DEFAULT" && ( */}
+    <View
+      style={[styles.swiperContainer, { position: "absolute", zIndex: 0 }]}
+    >
+      <View style={[styles.leftSwiper, { zIndex: 0 }]}>
+        <Swiper
+          ref={useSwiperL}
+          animateCardOpacity
+          // containerStyle={styles.container}
+          cards={photoCards}
+          renderCard={(card) => <Card card={card} />}
+          cardIndex={cardIndex}
+          backgroundColor="white"
+          stackSize={2}
+          infinite
+          showSecondCard
+          disableLeftSwipe
+          disableBottomSwipe
+          animateOverlayLabelsOpacity
+          onTapCard={() => {
+            setLeftEnlargedImage("TAPPED");
+            // setEnlargedImage("TAPPED");
+          }}
+          onSwipedRight={handleOnSwipedRight}
+          overlayLabels={{
+            right: {
+              title: "LIKE",
+              element: <OverlayLabel label="LIKE" color="#4CCC93" />,
+            },
+          }}
         />
-        <IconButton
-          name="heart"
-          onPress={handleOnSwipedLeft}
-          color="white"
-          backgroundColor="#E5566D"
+      </View>
+      <View style={(styles.rightSwiper, { zIndex: 0 })}>
+        <Swiper
+          ref={useSwiperR}
+          animateCardOpacity
+          // containerStyle={styles.container}
+          cards={photoCards2}
+          renderCard={(card) => <Card card={card} />}
+          cardIndex={cardIndex}
+          backgroundColor="white"
+          stackSize={2}
+          infinite
+          showSecondCard
+          disableRightSwipe
+          disableBottomSwipe
+          animateOverlayLabelsOpacity
+          onTapCard={() => {
+            setRightEnlargedImage("TAPPED");
+            // setEnlargedImage("TAPPED");
+          }}
+          onSwipedLeft={handleOnSwipedLeft}
+          overlayLabels={{
+            right: {
+              title: "LIKE",
+              element: <OverlayLabel label="NOPE" color="#E5566D" />,
+              style: {
+                wrapper: styles.overlayWrapper,
+              },
+            },
+            left: {
+              title: "LIKE",
+              element: <OverlayLabel label="LIKE" color="#4CCC93" />,
+            },
+          }}
         />
-        
-      </View> */}
-      {/* <View style={styles.copyright}>
-        <Text style={styles.copyright}>
-            All pictures were taken freely from Unsplash.com.
-            Names on the Photos are the names of photographers who took pictures.
-        </Text>
-      </View> */}
+      </View>
     </View>
-  );
-};
+  </View>
+  ); 
+}
+const Tab = createBottomTabNavigator();
 
-export default App;
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={LandingPage} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
