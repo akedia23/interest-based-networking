1c150a3db9bd226a7d84ecbabfc55131b81c50ad
Akash Kedia <kedia23akash@gmail.com>
2021-11-09 23:26:12 -0800

    Moved auth to frontend

---
+++ frontend/src/screens/SignOut.js
@@ -0,0 +1,31 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Text, View, TouchableOpacity, Alert } from "react-native";
+
+import styles from "../styles/Login.styles";
+import firebase from "../firebase/firebase";
+
+const SignOut = () => {
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then((resp) => {
+        console.log("Sign out successful.");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.loginBtn} onPress={signOut}>
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default SignOut;
